ae28e96193f157288832b63b413f9f43459cb6e5
Shaarif Asim <84714602+shaarifasim@users.noreply.github.com>
2022-08-20 19:37:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,849 @@
+// ch1
+
+// alert("Thanks for your input!");
+
+// ch2
+
+// var nationality;
+// nationality = "U.S.";
+
+// var lastName = "Smith";
+// var cityOfOrigin = "New Orleans";
+// var aussieGreeting = "g'Day";
+
+//  var thanx = "Thanks for your input!"
+//  alert(thanx);
+
+
+// ch3
+
+// var originalNum = 23;
+//  var numToBeAdded = 7;
+//  var newNum = originalNum + numToBeAdded;
+
+//  var originalNum = "23";
+//  var newNum = originalNum + 7;
+
+// ch4
+
+// userResponse
+// userResponseTime
+// userResponseTimeLimit
+// response
+
+// ch5
+
+// var popularNumber = 2 + 2;
+// var popularNumber = 12 - 24;
+// var popularNumber = 3 * 12;
+// var whatsLeftOver = 10 % 3;
+
+//  var num = 10;
+//  var anotherNum = 1;
+//  var popularNumber = num + anotherNum;
+
+
+// ch6
+//  var num = 1;
+//  var newNum = ++num;
+
+
+//  var num = 1;
+//  var newNum = num--;
+
+// ch7
+// var totalCost = 1 + 3 * 4;
+// var totalCost = 1 + (3 * 4);
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// resultOfComputation = (2 * 4) * (4 + 2);
+
+// ch8
+//  var message = "Thanks for your input!";
+//  alert(message);
+
+// var userName = "Shaarif";
+//  var message = "Thanks, ";
+//  var banger = "!";
+//  alert(message + userName + banger);
+
+// ch9
+// var spec = prompt("Your species?", "human");
+
+// var question = "Your species?";
+//  var defaultAnswer = "human";
+//  var spec = prompt(question, defaultAnswer);
+
+// ch10
+
+// var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+//  else{
+//     alert("Incorrect")
+//  }
+
+
+// var x = prompt ("what is the correct ans?");
+// var score = 0;
+// var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+//      userIQ = "genius";
+//      alert("Correct!");
+//      score++;
+//  }
+
+// ch11
+
+// if (fullName === "Mark" + " " + "Myers") {
+//     if (fullName === firstName + " " + "Myers") {
+//     if (fullName === firstName + " " + "Myers") {
+//     if (fullName === "firstName + " " + lastName) {
+//     if (totalCost === 81.50 + 135) {
+//     if (totalCost === materialsCost + 135) {
+//     if (totalCost === materialsCost + laborCost) {
+//     if (x + y === a - b) {}
+    
+
+// if (yourTicketNumber !== 487208) {
+//  alert("Better luck next time.");
+//     }
+
+// if (1 > 0) {
+//     if (0 < 1) {
+//     if (1 >= 0) {
+//     }}}
+
+// ch12
+
+//  var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+//  if (x !== "Vatican") {
+//  alert("Wrong answer");
+//  }
+
+// var correctAnswer = "Vatican";
+//  if (x === correctAnswer) {
+//  alert("Correct!");
+//  }
+//  else {
+//  score--;
+//  userIQ = "problematic";
+//  alert("Incorrect");
+// }
+
+// ch13
+// var a = prompt("enter weight ")
+// var b = prompt("enter time")
+// if (a > 300 && b < 6) {
+//      alert("Come to our tryout!");
+//      }
+//      else {
+//     alert("Come to our cookout!");
+//      }
+
+// if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//  alert("Come to our tryout!");
+//  }
+//      else {
+//     alert("Come to our cookout!");
+//     }
+
+
+// ch14
+// if (c === d) {
+//      if (x === y) {
+//      g = h;
+//      }
+//      else if (a === b) {
+//      g = h;
+//     }
+//      else {
+//      e = f;
+//      }
+//      }
+//      else {
+//      e = f;
+//      }
+
+// ch15
+// var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+// alert("Welcome to " + cities[3]);
+
+// ch16
+// var pets = ["dog","cat", "fish","lion"];
+// pets.pop(0);
+// alert(pets);
+// pets.push("dog","elephant");
+// alert(pets);
+
+// ch17
+// pets.shift();
+// pets.unshift("fish", "ferret");
+// pets.splice(2, 2, "pig", "duck", "emu");
+
+// ch18
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// if (cityToCheck === cleanestCities[0]) {
+//      alert("It's one of the cleanest cities");
+//      }
+//      else if (cityToCheck === cleanestCities[1]) {
+//      alert("It's one of the cleanest cities");
+//      }
+//      else if (cityToCheck === cleanestCities[2]) {
+//     alert("It's one of the cleanest cities");
+//      }
+//      else if (cityToCheck === cleanestCities[3]) {
+//      alert("It's one of the cleanest cities");
+//      }
+//      else if (cityToCheck === cleanestCities[4]) {
+//      alert("It's one of the cleanest cities");
+//      }
+    
+//  else {
+//  alert("It's not on the list");
+//      }
+
+// for (var i = 0; i <= 4; i++) {
+//      if (cityToCheck === cleanestCities[i]) {
+//      alert("It's one of the cleanest cities");
+//      }
+//  }
+
+// ch19
+
+// var matchFound = "no";
+//  for (var i = 0; i <= 4; i++);
+//  if (cityToCheck === cleanestCities[i]) {
+//  matchFound = "yes";
+//  alert("It's one of the cleanest cities");
+//  }
+//   if (matchFound === "no") {
+//  alert("It's not on the list");
+//  }
+
+// var matchFound = false;
+//  for (var i = 0; i <= 4; i++);
+//  if (cityToCheck === cleanestCities[i]) {
+//  matchFound = true;
+//  alert("It's one of the cleanest cities");
+//  }
+
+//  if (matchFound === false) {
+//  alert("It's not on the list");
+//  }
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// var cityToCheck = ["Cheyenne"];
+// var numElements = cleanestCities.length;
+//  var matchFound = false;
+//  for (var i = 0; i < numElements; i++);
+//  if (cityToCheck === cleanestCities[i]) {
+//  matchFound = true;
+//  alert("It's one of the cleanest cities");
+//  }
+//  if (matchFound === false) {
+//  alert("It's not on the list");
+//  }
+
+// ch20
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+//  var lastNames = ["Zzz", "Burp", "Dogbone", "Droop"];
+//  var fullNames = [];
+//  for (var i = 0; i < firstNames.length; i++) {
+//  for (var j = 0; j < lastNames.length; j++) {
+//  fullNames.push(firstNames[i] + lastNames[j]);
+
+//  }
+//  }
+
+// ch21
+
+// var cleanestCities = ["Cheyenne", "cheyenne", "Santa Fe", "santa fe", "Tucson", "tucson","greatfalls", "Honolulu", "honolulu"];
+
+// var cityToCheck = prompt("Enter your city");
+//  cityToCheck = cityToCheck.toLowerCase();
+//  var cleanestCities = ["cheyenne", "santa fe", "tucson", "great falls", "honolulu"];
+//  for (var i = 0; i <= 4; i++) {
+//  if (cityToCheck === cleanestCities[i]) {
+//  alert("It's one of the cleanest cities");
+//  }
+// }
+
+//  cityToCheck = cityToCheck.toUpperCase();
+
+// ch22
+// var firstChar = cityToCheck.slice(0, 1);
+// var someChars = cityToCheck.slice(2, 5);
+// var firstChar = cityToCheck.slice(0, 1);
+//  var otherChars = cityToCheck.slice(1);
+//  firstChar = firstChar.toUpperCase();
+//  otherChars = otherChars.toLowerCase();
+//  var cappedCity = firstChar + otherChars;
+
+
+//  var str = prompt("Enter some text");
+//  var numChars = str.length;
+//  for (var i = 0; i < numChars; i++) {
+// if (str.slice(i, i + 2) === " ") {
+//  alert("No double spaces!");
+//  break;
+
+//  }
+//  }
+
+// ch23
+
+//  for (var i = 0; i < text.length; i++) {
+//  if (text.slice(i, i + 12) === "World War II") {
+//  text = text.slice(0, i) + "the Second World War" + text.slice(i + 12);
+//  }
+//  }
+
+//ch 24
+
+// for (var i = 0; i < text.length; i++) {
+//      if (text.charAt(i) === "!") {
+//     alert("Exclamation point found!");
+//      break;
+//      }
+//     }
+
+// ch25
+
+//  var firstChar = text.indexOf("World War II");
+//  if (firstChar !== -1) {
+//  text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+//  }
+
+// text = text.replace("World War II", "the Second World War");
+
+// ch26
+// var numberOfStars = Math.round(scoreAvg);
+// var scoreAvg = Math.ceil(.000001);
+// var scoreAvg = Math.floor(.999999);
+
+// ch27
+// var randomNumber = Math.random();
+
+//  var bigDecimal = Math.random();
+//  var improvedNum = (bigDecimal * 6) + 1;
+//  var numberOfStars = Math.floor(improvedNum);
+
+// ch28
+// var currentAge = prompt("Enter your age.");
+//  var currentAge = prompt("Enter your age.");
+//  var yearsEligibleToVote = currentAge - 18;
+
+//  var profit = "200" - "duck";
+
+// ch29
+// var currentAge = prompt("Enter your age.");
+//  var qualifyingAge = parseInt(currentAge) + 1;
+//  alert(qualifyingAge);
+
+// var myFractional = parseFloat("1.9999");
+
+//  var integerString = "24"
+//  var num = Number(integerString);
+
+// var floatingNumString = "24.9876";
+//  var num = Number(floatingNumString);
+
+// var numberAsNumber = 1234;
+//  var numberAsString = numberAsNumber.toString();
+
+// ch30
+
+// var total = price + (price * taxRate);
+// var prettyTotal = total.toFixed(2);
+
+// ch31
+
+// var rightNow = new Date();
+//  var theDay = rightNow.getDay();
+
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//  var now = new Date();
+//  var theDay = now.getDay();
+//  var nameOfToday = dayNames[theDay];
+// alert(now);
+// alert(theDay);
+// alert(nameOfToday);
+
+// ch32
+//  var d = new Date();
+//  var currentMonth = d.getMonth();
+// getDate gives you a number for the day of the month.
+// var dayOfMonth = d.getDate();
+// getFullYear gives you a 4-digit number for the year.
+// var currYr = d.getFullYear();
+// 102
+// getHours gives you a number from 0 through 23 corresponding to midnight through 11
+// p.m.
+// var currentHrs = d.getHours();
+// getMinutes gives you a number from 0 through 59.
+// var currMins = d.getMinutes();
+// getSeconds gives you a number from 0 through 59.
+// var currSecs = d.getSeconds()
+// getMilliseconds gives you a number from 0 through 999.
+
+// ch33
+
+// var msDiff = new Date("June 30, 2035").getTime() - new Date().getTime();
+//  var daysTillDoom = Mathfloor(msDiff / (1000 * 60 * 60 * 24));
+
+// ch35
+
+// function tellTime() {
+//      var now = new Date();
+//      var theHr = now.getHours();
+//      var theMin = now.getMinutes();
+//      alert("Current time: "+ theHr + ":" + theMin);
+//      }
+
+// ch36
+
+// function greetUser(greeting) {
+//      alert(greeting);
+//      }
+//      greetUser("Hello, there.");
+
+// function greetUser(greeting) {
+//      alert(greeting);
+//      }
+   
+//      var whatever = "Hello, there.";
+//  greetUser(whatever);
+
+// ch37
+// var orderTot;
+//  if (merchTot >= 100) {
+//  orderTot = merchTot;
+//  }
+//  else if (merchTot < 50.01) {
+//  orderTot = merchTot + 5;
+//  }
+//  else {
+//  orderTot = merchTot + 5 + (.03 * (merchTot - 50));
+//  }
+
+// function calcTot(merchTot) {
+//      var orderTot;
+//      if (merchTot >= 100) {
+//      orderTot = merchTot;
+//      }
+//      else if (merchTot < 50.01) {
+//      orderTot = merchTot + 5;
+//      }
+//      else {
+//      orderTot = merchTot + 5 + (.03 * (merchTot - 50));
+//      }
+//      return orderTot;
+//      }
+
+// ch38
+
+// var theSum
+//  addNumbers();
+
+//  function addNumbers() {
+//      theSum = 2 + 2;
+//     }
+// alert(theSum)
+
+// ch39
+
+// switch(dayOfWk) {
+//      case "Sat" :
+//      alert("Whoopee");
+//      break;
+//      case "Sun" :
+//      alert("Whoopee");
+//      break;
+//      case "Fri" :
+//      alert("TGIF!");
+//      break;
+//      default :
+//      alert("Shoot me now!");
+//      }
+
+// ch40
+
+// default :
+//  alert("Shoot me now!");
+// } 
+// ch41
+
+// var i = 0;
+//  while (i <= 3) {
+//  alert(i);
+//  i++;
+//  }
+
+// 42
+
+// var i = 0;
+//  do {
+//  alert(i);
+//  i++;
+//  }
+//   while (i <= 3);
+
+// 43
+/* <script>
+ function sayHi() {
+ alert("Hello world!");
+ }
+ function sayBye() {
+ alert("Buh-bye!");
+ }
+ </script> */
+
+//  44
+var animals = []; // Declare an empty array
+// 45
+/* <a href="#" onClick="alert('Hi');">Click</a> */
+
+// function popup(message) {
+//      alert(message);
+//      }
+
+    //  <a href="JavaScript:void(0)" onClick="popup('Hi');">Click</a> 
+
+
+
+// 46
+// img src="button-greet.png" onClick="alert('Hello world!');">
+/* <img src="button-greet.png" onClick="greetTheUser();"> */
+
+// 47
+/* <img src="before-pic.jpg" onMouseover="src='after-pic.jpg'"></img> */
+/* <h1 onMouseover="alert('Be sure to get your shopping done today.');">World Ends Tomorrow</h1> */
+
+// 48
+// Email:<br>
+/* <input type="text" size="30" onFocus="this.style.backgroundColor='yellow';"></input> */
+// Email:<br>
+// <input type="text" size="30" onFocus="this.style.backgroundColor='yellow';"
+// onBlur="this.style.backgroundColor='white';">
+
+// 49
+/* <form>
+Email:
+<input type="text">
+<input type="submit" value="Submit">
+</form> */
+
+/* <form>
+Email:
+<input type="text" id="email">
+<input type="submit" value="Submit">
+</form>
+
+<form onSubmit="checkAddress('email');">
+Email:
+<input type="text" id="email">
+<input type="submit" value="Submit">
+</form> */
+
+// 50
+/* <form>
+ZIP:<br>
+<input type="text" id="zip" onBlur="fillCity();"><br>
+City:<br>
+<input type="text" id="city">
+</form>
+
+function fillCity() {
+ var cityName;
+ var zipEntered = document.getElementById("zip").value;
+ switch (zipEntered) {
+ case "60608" :
+ cityName = "Chicago";
+ break;
+ case "68114" :
+ cityName = "Omaha";
+ break;
+ case "53212" :
+ cityName = "Milwaukee";
+ }
+ document.getElementById("city").value = cityName;
+ } */
+
+//  ch51
+/* <p id="slowLoris">
+Slow lorises are a group of several species of strepsirrhine primates which make up the genus Nycticebus.
+<a href="javascript:void(0);" onClick="expandLoris();"><em>Click for more.</em></a>
+</p>
+
+function expandLoris() {
+     var expandedParagraph = "Slow lorises are a group of several species of trepsirrhine primates which
+     make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety 
+     distinctive coloration patterns that are species-dependent. The hands and feet  slow lorises have several
+     adaptations that give them a pincer-like grip and enable them to grasp branches for long periods  time.
+     Slow lorises have a toxic bite, a rare trait among mammals.";
+     document.getElementById("slowLoris").innerHTML = expandedParagraph;
+     }
+     */
+
+    //  function placeAList() {
+    //      var listToPlace = "<ol><li>Slow loris</li><li>Fast loris</li><li>Just-right loris</li></ol>";
+    //      document.getElementById("lorisList").innerHTML = listToPlace;
+    //      }
+         
+    //      function peekAtContent() {
+    //          var whatsThere = document.getElementById("content").innerHTML;
+    //         }
+
+    // ch52
+
+    // <img src="blobfish.jpg" id="ugly" onClick="makeInvisible();"></img>
+    // function makeInvisible() {
+    //      document.getElementById("ugly").className = "hidden";
+    //      }
+    
+    // 53
+
+    // <img src="before-pic.jpg" id="before" onMouseover="swapPic();"></img>
+    // function swapPic() {
+    //      document.getElementById("before").src = "after-pic.jpg";
+    //      }
+
+    // 54
+    // function swapPic() {
+    //      document.getElementById("before").src = "after-pic.jpg";
+    //      }
+    // function swapPic() {
+    //      var pic = document.getElementById("before");
+    //      pic.src = "after-pic.jpg";
+    //      }
+
+    // 55
+    // function makeBig() {
+    //      document.getElementById("p1").className += " big";
+    //      }
+    // function makeBig() {
+    //      document.getElementById("p1").style.fontSize = "2em";
+    //      }        
+
+    // 56
+    // var par = document.getElementsByTagName("p");
+
+//     <p>This bed is too small.</p>
+// <p>This bed is too big.</p>
+// <p>This bed is just right.</p>
+
+// par[1].innerHTML = "This SUV is too big.";
+
+// for (var i = 0; i < par.length; i++) {
+//      par[i].style.fontFamily = "Verdana, Geneva, sans-serif";
+//      }
+// var pics = document.getElementsByTagName("img");
+// 57
+
+
+
+// var pars = document.getElementsByTagName("p");
+// var textInMiddleParagraph = pars[1].innerHTML;
+// var e = document.getElementByID("rules");
+//  var paragraphs = e.getElementsByTagName("p");
+
+
+// var t = document.getElementById("table9");
+//  var cells = t.getElementsByTagName("td");
+//  for (var i = 0; i < cells.length; i++) {
+//  cells[i].style.backgroundColor = "pink";
+//  }
+
+
+// 58 DOM
+
+
+// 1st level: document
+// 2nd level: <html>
+// 3rd level: <head>
+// 4th level: <title>
+// 5th level: Simple document
+// </title>
+// </head>
+// 3rd level <body>
+// 4th level <p>
+// 5th level There's not much to this.
+// </p>
+// </body>
+// </html>
+
+
+// ch 60
+
+// var eField = document.getElementById("email");
+// var eField = document.getElementsByTagName("p");
+// var contents = p[2].innerHTML;
+
+// var d = document.getElementById("div3");
+//  var p = d.getElementsByTagName("p");
+//  var contents = p[2].innerHTML;
+
+
+// 61 The DOM: Junk artifacts and nodeType
+
+// var nType = targetNode.nodeType;
+
+// 62 The DOM: More ways to target elements
+
+// var targetNode = parentNode.childNodes[0];
+// var targetNode = parentNode.firstChild;
+// var targetNode = parentNode.lastChild;
+
+// chap 63 The DOM: Getting a target's name
+
+// var parent = document.getElementById("div1");
+// var target = parent.firstChild;
+// nName = target.nodeName;
+
+// chap 64 The DOM: Counting elements
+
+// var parentNode = document.getElementById("d1");
+// var nodeList = parentNode.childNodes;
+
+// chap 65 The DOM: Attributes
+
+// var target = document.getElementById("p1");
+// var hasClass = target.hasAttribute("class");
+
+// var target = document.getElementById("div1");
+// var attVal = target.getAttribute("class");
+
+// chap 66 The DOM: Attribute names and values
+
+// var list = document.getElementById("p1").attributes;
+// var numOfItems = document.getItemById("p1").attributes.length;
+
+// 67 The DOM: Adding nodes
+
+// var nodeToAdd = document.createElement("p");
+// nodeToAdd.setAttribute("class", "regular");
+// var imgNodeToAdd = document.createElement("img");
+// imgNodeToAdd.setAttribute("border", "1");
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
